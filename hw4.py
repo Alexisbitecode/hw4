767aef429ecf5e8f62ba33e74e5bc8f94ee40351
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-28 08:50:21 -0700

    Create hw4.py

---
+++ hw4.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns  # Add this import statement
+
+# Load the data from a CSV file
+df = pd.read_csv("StudentsPerformance.csv")
+df1 = df.groupby("parental level of education").mean()
+
+fig = plt.figure(figsize=(10, 6))
+sns.scatterplot(x="math score", y="reading score", hue="parental level of education", data=df1)
+plt.xlabel("Math Score")
+plt.ylabel("Reading Score")
+plt.title("Scatter Plot of Math Score vs. Reading Score (Colored by Parental Education)")
+# Display the plot in Streamlit
+st.pyplot(fig)
