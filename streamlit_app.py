c2047a62cd74cf7e3f9897fb11bec70fcc02a415
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-28 09:45:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns  # Add this import statement
 
+st.subheader("First Graph")
+
 # Load the data from a CSV file
 df = pd.read_csv("StudentsPerformance.csv")
 df1 = df.groupby("parental level of education")[["math score", "reading score"]].mean()
@@ -20,6 +22,8 @@ st.pyplot(fig)
 st.write("""
 We can see from the graph that students' math and reading scores are positively related to the parental level of education. The higher the parental level of education, the higher grades their kids can get. This quite makes sense since parents play a pretty important role in kids' education.
 """)
+
+st.subheader("Second Table")
 
 # Second table
 df2=df.groupby("test preparation course")[["math score", "reading score"]].mean()
